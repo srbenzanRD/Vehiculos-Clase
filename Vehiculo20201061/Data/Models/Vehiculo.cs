55fc3331d6c35baa0985012182cbc878ea519c85
FranciscoBelen <117422836+FranciscoBelen@users.noreply.github.com>
2022-11-04 05:26:32 +0100

    Add files via upload
    Espero que funcione bien

---
+++ Vehiculo20201061/Data/Models/Vehiculo.cs
@@ -0,0 +1,11 @@
+namespace Vehiculo20201061;
+
+public class Vehiculo
+{
+    public int VehiculoID { get; set; }
+    public string Marca { get; set; } = null!;
+    public string  Modelo { get; set; } = null!;
+    public int Año { get; set; }
+    public string Color { get; set; } = null!;
+
+}
